15915f35ddc4dc3700e98cd4e14286f4b995ca5a
Zhaoyang Dai <zhaoyang-dai@uiowa.edu>
2013-10-25 21:43:01 -0500

    Contact Us

--- ContactUs.aspx.vb
+++ ContactUs.aspx.vb
@@ -1,19 +1,27 @@
 ﻿Imports System.Net.Mail
+Imports Microsoft.VisualBasic
 Partial Class ContactUs
     Inherits System.Web.UI.Page
 
-    'Protected Sub SendMessageBtn_Click(sender As Object, e As EventArgs) Handles SendMessageBtn.Click
-    '    Dim emailMessage As New MailMessage()
-    '    Try
-    '        emailMessage.From = New MailAddress(TBEmail.Text)
-    '        emailMessage.To.Add("test@test.com")
-    '        emailMessage.Subject = "Question"
-    '        emailMessage.Body = TBMessage.Text
+    Protected Sub SendMessageBtn_Click(sender As Object, e As EventArgs) Handles SendMessageBtn.Click
+        Dim emailMessage As New MailMessage()
+        Try
+            emailMessage.From = New MailAddress("ui.6k183@gmail.com")
+            emailMessage.To.Add("michael-colbert@uiowa.edu")
+            emailMessage.Subject = "Question"
+            emailMessage.Body = "This message is send from " & TBEmail.Text & ". Here is the message text. " & TBMessage.Text
+            Dim smtp As New SmtpClient("smtp.gmail.com")
+            smtp.Port = 587
+            smtp.EnableSsl = True
+            smtp.Credentials = New System.Net.NetworkCredential("ui.6k183@gmail.com", "6k183U*I")
+            smtp.Send(emailMessage)
 
-    '    Catch ex As Exception
+        Catch ex As Exception
 
-    '    End Try
+        End Try
+        MsgBox("Your message has been sent. Administrator will reply you in next 24 hrs through email.", MsgBoxStyle.OkOnly, "Sent!")
+        TBEmail.Text = String.Empty
+        TBMessage.Text = String.Empty
 
-
-    'End Sub
+    End Sub
 End Class
